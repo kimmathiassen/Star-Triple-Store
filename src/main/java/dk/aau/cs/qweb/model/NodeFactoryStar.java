f2c52ad98dc5e92a24818eea68f4a003390d7141
kimajakobsen <kimajakobsen@gmail.com>
2017-01-31 14:36:36 +0100

    Test case for embedded triples in turtle format

--- src/main/java/dk/aau/cs/qweb/model/NodeFactoryStar.java
+++ src/main/java/dk/aau/cs/qweb/model/NodeFactoryStar.java
@@ -5,7 +5,7 @@ import org.apache.jena.graph.NodeFactory;
 
 public class NodeFactoryStar extends NodeFactory {
 
-	public Node createEmbeddedNode(Node node1, Node node2, Node node3) {
+	public static Node createEmbeddedNode(Node node1, Node node2, Node node3) {
 		return new Node_Triple(node1,node2,node3);
 	}
 }
---
+++ src/test/java/dk/aau/cs/qweb/TurtleStarReaderEmbeddedTest.java
@@ -0,0 +1,64 @@
+package dk.aau.cs.qweb;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import dk.aau.cs.qweb.graph.Graph;
+import dk.aau.cs.qweb.input.TurtleStarReader;
+import dk.aau.cs.qweb.model.NodeFactoryStar;
+
+public class TurtleStarReaderEmbeddedTest {
+
+	static Graph g;
+	
+	@BeforeClass
+	public static void setup() {
+		g = new Graph();
+        Model model = ModelFactory.createModelForGraph(g);
+		TurtleStarReader reader = new TurtleStarReader(model);
+		
+		File spiderman = new File("src/test/resources/TurtleStar/david.ttls");
+		try {
+			reader.read(spiderman);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void blankNodeObject() {
+		Node s1 = NodeFactory.createURI("http://example.org/kim");
+		Node p1 = NodeFactory.createURI("http://example.org/worksAt"); 
+		Node o1 = NodeFactory.createURI("http://example.org/aau");
+		Node subject = NodeFactoryStar.createEmbeddedNode(s1, p1, o1);
+		Node predicate = NodeFactory.createURI("http://example.org/is");
+		Node object = Node.ANY;
+		
+		Triple triplePattern = new Triple(subject,predicate,object );
+		
+		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
+		int count = 0;
+		
+		while (iterator.hasNext()) {
+			iterator.next();
+			count++;
+		}
+		
+		assertEquals(count,1);
+	}
+	
+}
