678914cf8aaedbfac0d3535534a56e5b26e8087f
kimajakobsen <kimajakobsen@gmail.com>
2017-02-02 19:58:31 +0100

    clean up

--- src/main/java/dk/aau/cs/qweb/triplestore/TripleStoreIterator.java
+++ src/main/java/dk/aau/cs/qweb/triplestore/TripleStoreIterator.java
@@ -9,38 +9,19 @@ import dk.aau.cs.qweb.triple.TripleStar;
 
 public class TripleStoreIterator extends WrappedIterator<TripleStar> {
 	
-	protected Index X;
-    protected Index A;
-    protected Index B;
     protected Graph toNotify;
     protected TripleStar current;
 
 	public TripleStoreIterator(
 			Graph toNotify, 
-			Iterator<TripleStar> it, 
-	        Index X, 
-	        Index A, 
-	        Index B ) {
+			Iterator<TripleStar> it
+	        ) {
 		super( it ); 
-        this.X = X;
-        this.A = A; 
-        this.B = B; 
         this.toNotify = toNotify;
 	}
-	
-
-//	@Override 
-//	public void remove() {
-//	    super.remove();
-//	    X.removedOneViaIterator();
-//	    A.remove( current );
-//	    B.remove( current );
-//	    toNotify.getEventManager().notifyDeleteTriple( toNotify, current);
-//    }
 	
 	@Override
     public TripleStar next() {
         return current = super.next();       
     }
-
 }
