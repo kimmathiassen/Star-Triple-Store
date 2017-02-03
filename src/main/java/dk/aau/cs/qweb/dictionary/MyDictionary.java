82c35c8008c176ba7fc418ee78ecc886b9cf4d3b
kimajakobsen <kimajakobsen@gmail.com>
2017-02-03 17:35:37 +0100

    addressed a bug that occured when an embedded triple was added to the dict. It would always be registered again even if it already existed in the dict.

--- src/main/java/dk/aau/cs/qweb/dictionary/MyDictionary.java
+++ src/main/java/dk/aau/cs/qweb/dictionary/MyDictionary.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 
+import dk.aau.cs.qweb.helper.BitHelper;
+import dk.aau.cs.qweb.model.NodeFactoryStar;
 import dk.aau.cs.qweb.model.Node_Triple;
-import dk.aau.cs.qweb.triple.TripleStar;
 import dk.aau.cs.qweb.triple.Key;
 import dk.aau.cs.qweb.triple.KeyFactory;
-import dk.aau.cs.qweb.triple.TriplePatternBuilder;
 import dk.aau.cs.qweb.triple.TriplePattern;
+import dk.aau.cs.qweb.triple.TriplePatternBuilder;
+import dk.aau.cs.qweb.triple.TripleStar;
 
 public class MyDictionary {
 	
@@ -81,70 +83,56 @@ public class MyDictionary {
 		return builder.createTriplePatter();
 	}
 
-	private Key getKey(Node node) {
+	private Key getKey(Node node) { //Only used in triple patterns 
 		if (node2Id.containsKey(node)) {
 			return node2Id.get(node);
 		} else {
-			return  new Key(0);
+			return  new Key(0); //meaning that a resource does not exist in dict, key 0 is an error code.
 		}
 	}
 
 
 	public TripleStar createTriple(Triple t) {
-		Key subject;
-		Key predicate;
-		Key object;
-		
-		if (node2Id.containsKey(t.getSubject())) {
-			subject = node2Id.get(t.getSubject());
-		} else {
-			subject = registerNode(t.getSubject());
-		}
-	
-		if (node2Id.containsKey(t.getPredicate())) {
-			predicate = node2Id.get(t.getPredicate());
-		} else {
-			predicate = registerNode(t.getPredicate());
-		}
-	
-		if (node2Id.containsKey(t.getObject())) {
-			object = node2Id.get(t.getObject());
-		} else {
-			object = registerNode(t.getObject());
-		}
-		
+		Key subject = nodeToKey(t.getSubject());
+		Key predicate = nodeToKey(t.getPredicate());
+		Key object = nodeToKey(t.getObject());
+				
 		return new TripleStar(subject,predicate,object);
 	}
 
+	private Key nodeToKey(Node node) {
+		if (node instanceof Node_Triple) {
+			Node_Triple embeddedNode = (Node_Triple) node;
+			Key s1 = registerOrGetNode(embeddedNode.getSubject());
+			Key p1 = registerOrGetNode(embeddedNode.getPredicate());
+			Key o1 = registerOrGetNode(embeddedNode.getSubject());
+			
+			return KeyFactory.createKey(s1, p1, o1);
+		} else {
+			return registerOrGetNode(node);
+		}
+	}
+
+	private Key registerOrGetNode(Node node) {
+		if (node2Id.containsKey(node)) {
+			return node2Id.get(node);
+		} else {
+			return registerNode(node);
+		}
+	}
 
 	public Node getNode(Key id) {
-		return id2Node.get(id);
+		if (BitHelper.isIdAnEmbeddedTriple(id)) {
+			Key subject = KeyFactory.createKey(BitHelper.getEmbeddedSubject(id));
+			Key predicate = KeyFactory.createKey(BitHelper.getEmbeddedPredicate(id));
+			Key object = KeyFactory.createKey(BitHelper.getEmbeddedObject(id));
+			return NodeFactoryStar.createEmbeddedNode(id2Node.get(subject), id2Node.get(predicate), id2Node.get(object));
+		} else {
+			return id2Node.get(id);
+		}
 	}
 	
-	private Key registerNode(Node node){
-		Key tempId;
-		if (node instanceof Node_Triple) {
-			tempId = registerEmbeddedKey(node);
-		} else {
-			tempId = registerKey(node);
-		}
-		return tempId;
-	}
-
-	private Key registerEmbeddedKey(Node node) {
-		Node_Triple embeddedNode = (Node_Triple) node;
-		
-		Key subject = registerKey(embeddedNode.getSubject());
-		Key predicate = registerKey(embeddedNode.getPredicate());
-		Key object = registerKey(embeddedNode.getObject());
-		
-		Key embeddedTriple = KeyFactory.createKey(subject.getId(), predicate.getId(), object.getId());
-//		id2Node.put(embeddedTriple, node);
-//		node2Id.put(node,embeddedTriple);
-		return embeddedTriple;
-	}
-
-	private Key registerKey(Node node) {
+	private Key registerNode(Node node) {
 		Key tempId;
 		Key key = new Key(id);
 		id2Node.put(key, node);
