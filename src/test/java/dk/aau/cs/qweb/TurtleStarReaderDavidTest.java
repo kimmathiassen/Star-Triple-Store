e9e10caa32c3917773bd31a559d1a166dd7ca2b6
kimajakobsen <kimajakobsen@gmail.com>
2017-02-15 17:12:36 +0100

    update to use full stack.

--- src/test/java/dk/aau/cs/qweb/TurtleStarReaderDavidTest.java
+++ src/test/java/dk/aau/cs/qweb/TurtleStarReaderDavidTest.java
@@ -2,67 +2,83 @@ package dk.aau.cs.qweb;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Iterator;
-
-import org.apache.jena.graph.Node;
-import org.apache.jena.graph.NodeFactory;
-import org.apache.jena.graph.Triple;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import dk.aau.cs.qweb.graph.Graph;
 import dk.aau.cs.qweb.main.App;
+import dk.aau.cs.qweb.main.queryparser.SyntaxStar;
 
 public class TurtleStarReaderDavidTest {
 
 	static Graph g;
-
+	static String prolog;
+	static Model model;
+	
 	@BeforeClass
 	public static void setup() {
 		g = new Graph();
+		model = ModelFactory.createModelForGraph(g);
 		String filename = "src/test/resources/TurtleStar/david.ttls" ;
 
         App.registerTTLS();
+        App.registerQueryEngine();
+        
+        prolog = "PREFIX dc: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "+
+        		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
+        		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>  " + 
+        		"PREFIX ex: <http://example.org/>  " + 
+        		"PREFIX rel: <http://www.perceive.net/schemas/relationship/>  ";
     	
-        RDFDataMgr.read(g, filename);
+        RDFDataMgr.read(model, filename);
 	}
+
 	
 
 	@Test
 	public void blankNodeObject() {
-		Node subject = NodeFactory.createURI("http://www.w3.org/TR/rdf-syntax-grammar");
-		Node predicate = NodeFactory.createURI("http://example.org/stuff/1.0/editor");
-		Node object = Node.ANY;
-		
-		Triple triplePattern = new Triple(subject,predicate,object );
-		
-		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
-		int count = 0;
-		
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
+		String queryString = prolog +
+        		"SELECT ?o WHERE {<http://www.w3.org/TR/rdf-syntax-grammar> <http://example.org/stuff/1.0/editor> ?o .}" ; 
+       
+	    Query query = QueryFactory.create(queryString,SyntaxStar.syntaxSPARQL_Star) ;
+	    int count = 0;
+	    
+	    try(QueryExecution qexec = QueryExecutionFactory.create(query, model)){
+	        ResultSet results = qexec.execSelect() ;
+	        
+	        while ( results.hasNext() ) {
+	            results.next();
+	            count++;
+	        }
+	    }
 		
 		assertEquals(1,count);
 	}
 	
 	@Test
 	public void blankNodeSubject() {
-		Node subject = Node.ANY;
-		Node predicate = NodeFactory.createURI("http://example.org/stuff/1.0/fullname");
-		Node object = NodeFactory.createLiteral("Dave Beckett");
-		
-		Triple triplePattern = new Triple(subject,predicate,object );
-		
-		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
-		int count = 0;
-		
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
+		String queryString = prolog +
+        		"SELECT ?s WHERE {?s <http://example.org/stuff/1.0/fullname> \"Dave Beckett\" .}" ; 
+       
+	    Query query = QueryFactory.create(queryString,SyntaxStar.syntaxSPARQL_Star) ;
+	    int count = 0;
+	    
+	    try(QueryExecution qexec = QueryExecutionFactory.create(query, model)){
+	        ResultSet results = qexec.execSelect() ;
+	        
+	        while ( results.hasNext() ) {
+	            results.next();
+	            count++;
+	        }
+	    }
 		
 		assertEquals(1,count);
 	}
--- src/test/java/dk/aau/cs/qweb/TurtleStarReaderSpidermanTest.java
+++ src/test/java/dk/aau/cs/qweb/TurtleStarReaderSpidermanTest.java
@@ -2,85 +2,100 @@ package dk.aau.cs.qweb;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Iterator;
-
-import org.apache.jena.graph.Node;
-import org.apache.jena.graph.NodeFactory;
-import org.apache.jena.graph.Triple;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import dk.aau.cs.qweb.graph.Graph;
 import dk.aau.cs.qweb.main.App;
+import dk.aau.cs.qweb.main.queryparser.SyntaxStar;
 
 public class TurtleStarReaderSpidermanTest {
 	static Graph g;
+	static String prolog;
+	static Model model;
 
 	@BeforeClass
 	public static void setup() {
 		g = new Graph();
 		String filename = "src/test/resources/TurtleStar/spiderman.ttls" ;
-
-        App.registerTTLS();
-    	
-        RDFDataMgr.read(g, filename);
+		model = ModelFactory.createModelForGraph(g);
+		 
+		App.registerTTLS();
+	    App.registerQueryEngine();
+	        
+        prolog = "PREFIX dc: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "+
+        		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
+        		"PREFIX foaf: <http://xmlns.com/foaf/0.1/>  " + 
+        		"PREFIX ex: <http://example.org/>  " + 
+        		"PREFIX rel: <http://www.perceive.net/schemas/relationship/>  ";
+        RDFDataMgr.read(model, filename);
 	}
 	
 
 	@Test
 	public void commaSeperatedTriples() {
-		Node subject = NodeFactory.createURI("http://example.org/spiderman");
-		Node predicate = NodeFactory.createURI("http://xmlns.com/foaf/0.1/name");
-		Node object = Node.ANY;
-		
-		Triple triplePattern = new Triple(subject,predicate,object );
-		
-		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
-		int count = 0;
-		
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
+		String queryString = prolog +
+        		"SELECT  ?o WHERE {ex:spiderman <http://xmlns.com/foaf/0.1/name> ?o .}" ; 
+       
+	    Query query = QueryFactory.create(queryString,SyntaxStar.syntaxSPARQL_Star) ;
+	    int count = 0;
+	    
+	    try(QueryExecution qexec = QueryExecutionFactory.create(query, model)){
+	        ResultSet results = qexec.execSelect() ;
+	        
+	        while ( results.hasNext() ) {
+	            results.next();
+	            count++;
+	        }
+	    }
 		
 		assertEquals(2,count);
 	}
 	
 	@Test
 	public void standardTriplePattern() {
-		Node subject = NodeFactory.createURI("http://example.org/spiderman");
-		Node predicate = NodeFactory.createURI("http://www.perceive.net/schemas/relationship/enemyOf");
-		Node object = Node.ANY;
-		
-		Triple triplePattern = new Triple(subject,predicate,object );
-		
-		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
-		int count = 0;
-		
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
-		
+		String queryString = prolog +
+        		"SELECT  ?o WHERE {ex:spiderman <http://www.perceive.net/schemas/relationship/enemyOf> ?o .}" ; 
+       
+	    Query query = QueryFactory.create(queryString,SyntaxStar.syntaxSPARQL_Star) ;
+	    int count = 0;
+	    
+	    try(QueryExecution qexec = QueryExecutionFactory.create(query, model)){
+	        ResultSet results = qexec.execSelect() ;
+	        
+	        while ( results.hasNext() ) {
+	            results.next();
+	            count++;
+	        }
+	    }
+	    
 		assertEquals(1,count);
 	}
 	
 	@Test
 	public void linesWithComments() {
-		Node subject = Node.ANY;
-		Node predicate = NodeFactory.createURI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
-		Node object = NodeFactory.createURI("http://xmlns.com/foaf/0.1/person");
-		
-		Triple triplePattern = new Triple(subject,predicate,object );
-		
-		Iterator<Triple> iterator = g.graphBaseFind(triplePattern);
-		int count = 0;
-		
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
+		String queryString = prolog +
+        		"SELECT  ?o WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/person> .}" ; 
+       
+	    Query query = QueryFactory.create(queryString,SyntaxStar.syntaxSPARQL_Star) ;
+	    int count = 0;
+	    
+	    try(QueryExecution qexec = QueryExecutionFactory.create(query, model)){
+	        ResultSet results = qexec.execSelect() ;
+	        
+	        while ( results.hasNext() ) {
+	            results.next();
+	            count++;
+	        }
+	    }
 		
 		assertEquals(2,count);
 	}
