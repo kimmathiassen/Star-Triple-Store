c49b7a97f580f827b730c5b8d6060bc0d8922a0d
kimajakobsen <kimajakobsen@gmail.com>
2017-02-17 14:41:13 +0100

    add iterators for OpTriple

--- src/main/java/dk/aau/cs/qweb/queryengine/OpStarExecutor.java
+++ src/main/java/dk/aau/cs/qweb/queryengine/OpStarExecutor.java
@@ -2,14 +2,18 @@ package dk.aau.cs.qweb.queryengine;
 
 import java.util.Iterator;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.sparql.algebra.op.OpAssign;
 import org.apache.jena.sparql.algebra.op.OpBGP;
+import org.apache.jena.sparql.algebra.op.OpJoin;
+import org.apache.jena.sparql.algebra.op.OpTriple;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.QueryIterator;
+import org.apache.jena.sparql.engine.join.Join;
 import org.apache.jena.sparql.engine.main.OpExecutor;
 import org.apache.jena.sparql.engine.main.OpExecutorFactory;
 
-import dk.aau.cs.qweb.graph.Graph;
 import dk.aau.cs.qweb.triple.TriplePatternBuilder;
 import dk.aau.cs.qweb.triple.TripleStarPattern;
 
@@ -36,20 +40,52 @@ public class OpStarExecutor extends OpExecutor{
 	@Override
 	public QueryIterator execute ( OpBGP opBGP, QueryIterator input )
 	{
-		if (    opBGP.getPattern().isEmpty()
-		     || ! (execCxt.getDataset().getDefaultGraph() instanceof Graph) )
-		{
-			return super.execute( opBGP, input );
-		}
-
-
+		throw new IllegalArgumentException("This iterator should never be called! All BGPs should have been rewritten. opBGP: "+opBGP.toString());
+		//TODO this should never happen
+//		if (    opBGP.getPattern().isEmpty()
+//		     || ! (execCxt.getDataset().getDefaultGraph() instanceof Graph) )
+//		{
+//			return super.execute( opBGP, input );
+//		}
+//
+//
+//		Iterator<SolutionMapping> qIt = new EncodeBindingsIterator( input, execCxt );
+//		for ( Triple t : opBGP.getPattern().getList() ) {
+//			qIt = new TriplePatternQueryIter( encode(t), qIt, execCxt );
+//		}
+//
+//		return new DecodeBindingsIterator( qIt, execCxt );
+	}
+	
+	@Override
+	public QueryIterator execute ( OpTriple opTriple, QueryIterator input )
+	{
 		Iterator<SolutionMapping> qIt = new EncodeBindingsIterator( input, execCxt );
-		for ( Triple t : opBGP.getPattern().getList() ) {
-			qIt = new TriplePatternQueryIter( encode(t), qIt, execCxt );
-		}
+		qIt = new TriplePatternQueryIter( encode(opTriple.getTriple()), qIt, execCxt );
 
 		return new DecodeBindingsIterator( qIt, execCxt );
 	}
+	
+	@Override
+	public QueryIterator execute ( OpAssign opAssign, QueryIterator input )
+	{
+		throw new NotImplementedException("OpStarExecutor.execute(OpAssign) not implemented ");
+	}
+	
+	@Override
+	public QueryIterator execute ( OpJoin opJoin, QueryIterator input )
+	{
+        // Need to clone input into left and right.
+        // Do by evaling for each input case, the left and right and concat'ing
+        // the results.
+
+        QueryIterator left = exec(opJoin.getLeft(), input) ;
+        QueryIterator right = exec(opJoin.getRight(), root()) ;
+        // Join key.
+        QueryIterator qIter = Join.join(left, right, execCxt) ;
+        return qIter ;
+	}
+	
 	
 	// helper methods
 
