850a87d1b4df2b00eef1dd6cd8551434e5dd92b9
kimajakobsen <kimajakobsen@gmail.com>
2017-03-28 11:25:07 +0200

    clean up in test

--- src/test/java/dk/aau/cs/qweb/turtlestar/TurtleStarReaderEmbeddedTest.java
+++ src/test/java/dk/aau/cs/qweb/turtlestar/TurtleStarReaderEmbeddedTest.java
@@ -6,7 +6,6 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
