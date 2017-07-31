4881a84a2a8ec699603b7ddea5cb90d2f0e24668
kim <kim@kim-ThinkPad-X240>
2017-07-31 08:27:31 +0200

    preparation for dynamic s p o size

--- src/main/java/dk/aau/cs/qweb/main/Config.java
+++ src/main/java/dk/aau/cs/qweb/main/Config.java
@@ -3,6 +3,9 @@ package dk.aau.cs.qweb.main;
 public class Config {
 
 	private static boolean prefixDictionaryState = true;
+	private static int subjectSize = 20;
+	private static int predicateSize = 20;
+	private static int objectSize = 20;
 	
 	public static void enablePrefixDictionary(boolean flag) {
 		prefixDictionaryState = flag;
@@ -10,5 +13,17 @@ public class Config {
 	
 	public static boolean isPrefixDictionaryEnabled() {
 		return prefixDictionaryState;
+	}
+
+	public static long getLargestSubjectId() {
+		return (long) Math.pow(2, subjectSize);
+	}
+	
+	public static long getLargestPredicateId() {
+		return (long) Math.pow(2, predicateSize);
+	}
+	
+	public static long getLargestObjectId() {
+		return (long) Math.pow(2, objectSize);
 	}
 }
