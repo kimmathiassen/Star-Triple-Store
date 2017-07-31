51bd8ae7358816256b9fad4b870fe93d1c490df3
kim <kim@kim-ThinkPad-X240>
2017-07-31 08:32:51 +0200

    ensure that elements of a reference triple is added to the index.

--- src/main/java/dk/aau/cs/qweb/triple/TripleStarBuilder.java
+++ src/main/java/dk/aau/cs/qweb/triple/TripleStarBuilder.java
@@ -7,25 +7,36 @@ import org.apache.jena.graph.Triple;
 
 import dk.aau.cs.qweb.dictionary.NodeDictionary;
 import dk.aau.cs.qweb.helper.BitHelper;
+import dk.aau.cs.qweb.node.Node_Triple;
 
 public class TripleStarBuilder {
 
 	public List<TripleStar> createTriple(Triple t) {
-		//There might be a problem here in regards to nodes being added multiple times
+		//There might be a problem here in regards to nodes being added multiple times, as in there is some code somewhere that does the same.
 		NodeDictionary dict = NodeDictionary.getInstance();
 		List<TripleStar> triples = new ArrayList<TripleStar>();
 		Key subject = dict.createKey(t.getSubject());
 		Key predicate = dict.createKey(t.getPredicate());
 		Key object = dict.createKey(t.getObject());
 		
-		if (BitHelper.isIdAnEmbeddedTriple(subject)) {
+		if (BitHelper.isReferenceBitSet(subject)) {
+			Key s1 = dict.createKey(((Node_Triple)t.getSubject()).getSubject());
+			Key p1 = dict.createKey(((Node_Triple)t.getSubject()).getPredicate());
+			Key o1 = dict.createKey(((Node_Triple)t.getSubject()).getObject());
+			triples.add(new TripleStar(s1,p1,o1));
+		} else if (BitHelper.isIdAnEmbeddedTriple(subject)) {
 			Key s1 = new Key(BitHelper.getEmbeddedSubject(subject));
 			Key p1 = new Key(BitHelper.getEmbeddedPredicate(subject));
 			Key o1 = new Key(BitHelper.getEmbeddedObject(subject));
 			triples.add(new TripleStar(s1,p1,o1));
 		}
 		
-		if (BitHelper.isIdAnEmbeddedTriple(object)) {
+		if (BitHelper.isReferenceBitSet(object)) {
+			Key s2 = dict.createKey(((Node_Triple)t.getObject()).getSubject());
+			Key p2 = dict.createKey(((Node_Triple)t.getObject()).getPredicate());
+			Key o2 = dict.createKey(((Node_Triple)t.getObject()).getObject());
+			triples.add(new TripleStar(s2,p2,o2)); 
+		} else if (BitHelper.isIdAnEmbeddedTriple(object)) {
 			Key s2 = new Key(BitHelper.getEmbeddedSubject(object));
 			Key p2 = new Key(BitHelper.getEmbeddedPredicate(object));
 			Key o2 = new Key(BitHelper.getEmbeddedObject(object));
